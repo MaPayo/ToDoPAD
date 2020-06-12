9d59ab467484f1535b326b135984fb08289090d5
MaPayo <marina.payorubio@gmail.com>
2020-06-12 14:11:09 +0200

    Prueba workservices

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 console.log(`Workbox ${workbox ? 'sí' : 'no'} está funcionando`)
 
 workbox.precaching.precacheAndRoute([
-  {url: '/index.html', revision: null },
-  {url: '/preact.js', revision: null},
-  {url: '/style.css', revision: null},
-  {url: '/functions.js', revision: null},
-  {url: '/VT323.ttf', revision: null},
-  {url: '/m.json', revision: null},
+  {url: '/ToDoPAD.github.io/index.html', revision: null },
+  {url: '/ToDoPAD.github.io/preact.js', revision: null},
+  {url: '/ToDoPAD.github.io/style.css', revision: null},
+  {url: '/ToDoPAD.github.io/functions.js', revision: null},
+  {url: '/ToDoPAD.github.io/VT323.ttf', revision: null},
+  {url: '/ToDoPAD.github.io/m.json', revision: null},
 ])
